49a118445cc996be316c5a231d770232022c8560
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-12 10:28:21 +0100

    Add basic button component and styles

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  background-color: ${(props) => props.theme.colorPrimary};
+  color: ${(props) => props.theme.backgroundColor};
+  padding: 0.2rem 2rem;
+  border: none;
+  border-radius: 30px;
+  margin: 20px;
+  font-size: 1.25rem;
+  font-weight: 600;
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    background-color: ${(props) => props.theme.featuredColor};
+  }
+`;
+
+export default ButtonStyled;
