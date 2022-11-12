e74bc2f578bbf30d02c069a84515a5f39951b14a
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-12 10:27:40 +0100

    Add main theme styles

---
+++ src/styles/mainTheme.tsx
@@ -0,0 +1,9 @@
+const mainTheme = {
+  colorPrimary: "#353535",
+  headerPaddingTop: "50px",
+  bodyPaddingHorizontal: "30px",
+  backgroundColor: "#fcfcfc",
+  featuredColor: "#6b3fbe",
+  paddingShort: "20px",
+};
+export default mainTheme;
