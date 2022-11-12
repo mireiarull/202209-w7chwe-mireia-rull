6d30e4c2766f96598c1df477aabef327ddc4823c
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-12 17:41:46 +0100

    Add login funcion in custom hook and useToken

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,13 +1,18 @@
-import { UserRegisterCredentials } from "../types";
+import { loginUserActionCreator } from "../redux/features/usersSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { UserCredentials, UserRegisterCredentials } from "../types";
+import decodeToken from "../utils/decodeToken";
 
 const useUser = () => {
+  const dispatch = useAppDispatch();
+
   const url = process.env.REACT_APP_API_SOCIAL!;
 
   const registerUser = async (userData: UserRegisterCredentials) => {
     await fetch(`${url}/users/register`, {
       method: "POST",
       body: JSON.stringify({
-        username: userData.userName,
+        username: userData.username,
         password: userData.password,
         email: userData.email,
       }),
@@ -17,7 +22,34 @@ const useUser = () => {
     });
   };
 
-  return { registerUser };
+  const loginUser = async (userData: UserCredentials) => {
+    try {
+      const responseData = await fetch(`${url}/users/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          username: userData.username,
+          password: userData.password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      const { token } = await responseData.json();
+
+      const userLogged = decodeToken(token);
+
+      dispatch(
+        loginUserActionCreator({
+          ...userLogged,
+          username: userData.username,
+          token: token,
+        })
+      );
+      localStorage.setItem("token", token);
+    } catch {}
+  };
+  return { registerUser, loginUser };
 };
 
 export default useUser;
