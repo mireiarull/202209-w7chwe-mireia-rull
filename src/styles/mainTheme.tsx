6bf52990c784fc5c79c8fd127ef9c473634ad1f2
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-15 21:13:04 +0100

    Reorganize protected routes files and folders

--- src/styles/mainTheme.tsx
+++ src/styles/mainTheme.tsx
@@ -2,7 +2,7 @@ const mainTheme = {
   colorPrimary: "#353535",
   headerPaddingTop: "50px",
   bodyPaddingHorizontal: "30px",
-  backgroundColor: "#fcfcfc",
+  backgroundColor: "#efefef",
   featuredColor: "#6b3fbe",
   paddingShort: "20px",
   primaryFont: "'Poppins', sans-serif",
