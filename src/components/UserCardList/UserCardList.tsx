61ea2bad05b32588bf5e2ffbe68b9bfe92960859
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-17 20:36:20 +0100

    Add modal for feedback after updating profile

--- src/components/UserCardList/UserCardList.tsx
+++ src/components/UserCardList/UserCardList.tsx
@@ -1,15 +1,18 @@
 import useApi from "../../hooks/useApi";
 import { useAppSelector } from "../../redux/hooks";
 import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
 import UserCard from "../UserCard/UserCard";
 import UserCardListStyled from "./UserCardListStyled";
 
 const UserCardList = (): JSX.Element => {
   const users = useAppSelector(({ users }) => users.list);
   const { loadRelations, loadAllUsersApi } = useApi();
+  const modal = useAppSelector(({ ui }) => ui.modal);
 
   return (
     <UserCardListStyled>
+      {modal && <Modal />}
       <div className="list-filter">
         <Button text="Show all" action={loadAllUsersApi} />
         <Button text="Show friends" action={() => loadRelations("friends")} />
